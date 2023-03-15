98431492f3ef15b8f884215aea1fe2cbed2acc6e
insert-username <insert-username@users.noreply.github.com>
2023-03-15 18:41:38 +0100

    Try to learn a bit by adding some tests.

---
+++ Frontier2D/FRONTIER-gnu/MFA/tst/ListTests.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "mfa1List.h"
+
+TEST(ListTests, testLen)
+{
+    auto lst = List<int>();
+
+    ASSERT_EQ(0, lst.returnLen());
+}
+
+TEST(ListTests, testLenWithElements)
+{
+    auto lst = List<int>();
+
+    lst.append(0);
+    ASSERT_EQ(1, lst.returnLen());
+
+    lst.append(0);
+    ASSERT_EQ(2, lst.returnLen());
+
+    lst.append(0);
+    ASSERT_EQ(3, lst.returnLen());
+}
+
+TEST(ListTests, testIndexFrom1)
+{
+    auto lst = List<int>();
+
+    lst.append(10);
+    lst.append(20);
+    lst.append(30);
+
+    ASSERT_EQ(10, lst.retrieve(1));
+    ASSERT_EQ(20, lst.retrieve(2));
+    ASSERT_EQ(30, lst.retrieve(3));
+}
